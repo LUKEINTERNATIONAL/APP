fc00ca050eb8df1c6906be2298a82b5840ae9093
Mwatha Bwanali <mwathabwanali@gmail.com>
2019-06-13 15:29:54 +0200

    changing VL reminder timelines

--- assets/js/vl.reminder.js
+++ assets/js/vl.reminder.js
@@ -1,12 +1,13 @@
 
 var arv_earliest_start_dates;
 
-function dateDiffInMonths(dt2, dt1) {
-  var months;
-  months = (dt2.getFullYear() - dt1.getFullYear()) * 12;
-  months -= dt1.getMonth() + 1;
-  months += dt2.getMonth();
-  return months <= 0 ? 0 : months;
+function dateDiffInMonths(d2, d1) {
+  var d1Y = d1.getFullYear();
+  var d2Y = d2.getFullYear();
+  var d1M = d1.getMonth();
+  var d2M = d2.getMonth();
+
+  return (d2M+12*d2Y)-(d1M+12*d1Y);
 }
 
 function getARTstartedDate() {
@@ -18,6 +19,7 @@ function getARTstartedDate() {
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && (this.status == 201 || this.status == 200)) {
       arv_earliest_start_dates = JSON.parse(this.responseText);
+      VLordered();
     }
   };
   xhttp.open("GET", url, true);
@@ -37,8 +39,8 @@ function calculateVLreminder() {
   var date1 = new Date(sessionStorage.sessionDate);
   var date2 = new Date(earliest_start_date);
 
-  //period_on_art = dateDiffInMonths(date1, date2);
-  if(sessionStorage.sessionDate == moment().format('YYYY-MM-DD'))
+  //The VL reminder is suppose to work in realtime only hence the following check 
+  //if(sessionStorage.sessionDate == moment().format('YYYY-MM-DD'))
     checkIFwithVLBounds();
 
 }
@@ -68,12 +70,12 @@ function checkIFwithVLBounds() {
   
   var time_bounds = [];
   time_bounds.push({start: 6, end: 12});
-  var cutoff = 24;
+  var cutoff = 12;
   var i = 0;
 
   while(i <= 36) {
-    time_bounds.push({start: (cutoff - 1), end: (cutoff + 12)});
-    cutoff += 24
+    time_bounds.push({start: (cutoff), end: (cutoff + 12)});
+    cutoff += 12
     i++
   }
 
@@ -82,6 +84,10 @@ function checkIFwithVLBounds() {
     for(var i = 0 ; i < time_bounds.length ; i++){
       if(period_on_art >= time_bounds[i].start && period_on_art <= time_bounds[i].end){
         vlAlert(period_on_art);
+        break;
+      }
+      if((period_on_art + 1) == time_bounds[i].start){
+        vlAlertReminder(period_on_art);
         break;
       }
     }
@@ -178,4 +184,170 @@ function cancelVLOrder() {
   document.getElementById('vl-cover-div').style = 'display: none;';
 }
 
-getARTstartedDate();
+function vlAlertReminder(period_on_art){
+  var popUpBox = document.getElementById('vl-popup-div');
+  var coverDIV = document.getElementById('vl-cover-div');
+
+  if(coverDIV == undefined) {
+    var coverDIV = document.createElement('div')
+    coverDIV.setAttribute('id','vl-cover-div');
+    var popUpBox = document.createElement('div')
+    popUpBox.setAttribute('id','vl-popup-div');
+
+    var hmtlBody = document.getElementsByTagName("body")[0];
+    hmtlBody.appendChild(popUpBox);
+    hmtlBody.appendChild(coverDIV);
+  }
+
+  coverDIV.style = 'display: inline;top: 0px;';
+  popUpBox.style  = 'background-color: lightyellow;display: inline;top: 10px;';
+  popUpBox.innerHTML = null;
+
+  var message = "VL check in one month time.<br />";
+  message += "It has been <b style='color: red;'>" + period_on_art + " </b>months since ART treatment<br />"
+  message += "was started on the <b style='color: black;'>";
+  message += moment(new Date(arv_earliest_start_dates.earliest_start_date)).format('DD/MMM/YYYY') + '</b>';
+
+  var p = document.createElement('p');
+  p.innerHTML = message;
+  cssText = 'text-align: center;color: green; font-weight: bold; font-size: 2.3em;';
+  cssText += 'margin-top: 10%;';
+  p.style = cssText;
+  popUpBox.appendChild(p);
+
+
+
+  /* ............... buttons ............................... */
+  var buttonContainer = document.createElement('div');
+  buttonContainer.setAttribute('class','buttonContainer');
+  popUpBox.appendChild(buttonContainer);
+
+  var buttonContainerRow = document.createElement('div');
+  buttonContainerRow.setAttribute('class','buttonContainerRow');
+  buttonContainer.appendChild(buttonContainerRow);
+
+  var cells = ['OK'];
+
+  for(var i = 0 ; i < cells.length ; i++){
+    var buttonContainerCell = document.createElement('div');
+    buttonContainerCell.setAttribute('class','buttonContainerCell');
+    buttonContainerCell.setAttribute('style','width: 100px;');
+    buttonContainerCell.innerHTML = cells[i];
+
+    buttonContainerCell.setAttribute('id','buttonContainerCell-blue');
+    buttonContainerCell.setAttribute('onmousedown','cancelVLOrder();');
+    buttonContainerRow.appendChild(buttonContainerCell);
+  }
+}
+
+function VLordered() {
+  var url = apiProtocol + "://" + apiURL + ":" + apiPort + "/api/v1";
+  url += '/observations?person_id=' + sessionStorage.patientID;
+  url += '&date=' + sessionStorage.sessionDate;
+  url += '&concept_id=8015';
+
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && (this.status == 201 || this.status == 200)) {
+      var obs = JSON.parse(this.responseText);
+      var obs_datetimes;
+      var obs_date;
+      var results_given;
+
+      for(var i = 0 ; i < obs.length ; i++){
+        console.log(obs[i].value_coded + "   " + obs[i].obs_datetime)
+        obs_date = new Date(  moment(obs[i].obs_datetime).format('YYYY-MM-DD') );
+        if(obs_datetimes == undefined){
+          obs_datetimes = obs_date;
+          results_given = obs[i].value_coded;
+        }else if (obs_date > obs_datetimes){
+          obs_datetimes = obs_date;
+          results_given = obs[i].value_coded;
+        }
+      }
+
+      if(results_given == undefined) {
+        calculateVLreminder();
+      }else if(parseInt(results_given) == 1065){
+        calculateVLreminder();
+      }else if(parseInt(results_given) == 1066){
+        var curr_date = new Date(sessionStorage.sessionDate);
+        var months_passed = dateDiffInMonths(curr_date, obs_datetimes);
+        
+        if(months_passed >= 3) {
+          calculateVLreminder();
+        }else{
+          askIfresultsAvailable(obs_datetimes);
+        }
+
+      }
+    }
+  };
+  xhttp.open("GET", url, true);
+  xhttp.setRequestHeader('Authorization', sessionStorage.getItem("authorization"));
+  xhttp.setRequestHeader('Content-type', "application/json");
+  xhttp.send();
+}
+
+function askIfresultsAvailable(obs_date) {
+  var popUpBox = document.getElementById('vl-popup-div');
+  var coverDIV = document.getElementById('vl-cover-div');
+
+  if(coverDIV == undefined) {
+    var coverDIV = document.createElement('div')
+    coverDIV.setAttribute('id','vl-cover-div');
+    var popUpBox = document.createElement('div')
+    popUpBox.setAttribute('id','vl-popup-div');
+
+    var hmtlBody = document.getElementsByTagName("body")[0];
+    hmtlBody.appendChild(popUpBox);
+    hmtlBody.appendChild(coverDIV);
+  }
+
+  coverDIV.style = 'display: inline;top: 0px;';
+  popUpBox.style  = 'background-color: lightyellow;display: inline;top: 10px;';
+  popUpBox.innerHTML = null;
+
+  var message = "VL test ordered on<br />";
+  message += "<b style='color: red;'>" + moment(obs_date).format('DD/MMM/YYYY') + "</b>";
+  message += "<br />Check if the results are available";
+
+  var p = document.createElement('p');
+  p.innerHTML = message;
+  cssText = 'text-align: center;color: green; font-weight: bold; font-size: 2.3em;';
+  cssText += 'margin-top: 10%;';
+  p.style = cssText;
+  popUpBox.appendChild(p);
+
+
+
+  /* ............... buttons ............................... */
+  var buttonContainer = document.createElement('div');
+  buttonContainer.setAttribute('class','buttonContainer');
+  popUpBox.appendChild(buttonContainer);
+
+  var buttonContainerRow = document.createElement('div');
+  buttonContainerRow.setAttribute('class','buttonContainerRow');
+  buttonContainer.appendChild(buttonContainerRow);
+
+  var cells = ['OK'];
+
+  for(var i = 0 ; i < cells.length ; i++){
+    var buttonContainerCell = document.createElement('div');
+    buttonContainerCell.setAttribute('class','buttonContainerCell');
+    buttonContainerCell.setAttribute('style','width: 100px;');
+    buttonContainerCell.innerHTML = cells[i];
+
+    //if(i == 0) {
+      buttonContainerCell.setAttribute('id','buttonContainerCell-blue');
+      buttonContainerCell.setAttribute('onmousedown','cancelVLOrder();');
+    /* }else if(i == 1) { 
+      buttonContainerCell.setAttribute('id','buttonContainerCell-red');
+      buttonContainerCell.setAttribute('onmousedown','cancelVLOrder();');
+    } */
+
+    buttonContainerRow.appendChild(buttonContainerCell);
+  }
+}
+
+//getARTstartedDate();
